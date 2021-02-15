532801fea65a0e96c2ce49d3a6cb766811d38200
daniel <danielvolosov@outlook.com>
2021-02-15 15:17:33 -0500

    Move zeroes video solution

---
+++ src/com/subtlealgorithm/strings_and_arrays/move_zeroes/Solution.java
@@ -0,0 +1,17 @@
+package com.subtlealgorithm.strings_and_arrays.move_zeroes;
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int pointer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0 ) {
+                int placeholder = nums[pointer];
+                nums[pointer] = nums[i];
+                nums[i] = placeholder;
+                pointer++;
+            }
+        }
+    }
+}
--- src/com/subtlealgorithm/strings_and_arrays/rotate_array/Solution.java
+++ src/com/subtlealgorithm/strings_and_arrays/rotate_array/Solution.java
@@ -3,14 +3,18 @@ package com.subtlealgorithm.strings_and_arrays.rotate_array;
 public class Solution {
 
     public void rotate(int[] nums, int k) {
+
         k = k % nums.length;
+
         reverseArray(nums, 0, nums.length - k - 1);
         reverseArray(nums, nums.length - k, nums.length - 1);
         reverseArray(nums, 0, nums.length - 1);
     }
 
     private void reverseArray(int[] nums, int start, int end) {
+
         while (start < end) {
+
             int placeholder = 0;
 
             placeholder = nums[start];

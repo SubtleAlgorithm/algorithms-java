eeb95a2c2fa5230cc8ce196cf202ea38f5ed6b20
daniel <danielvolosov@outlook.com>
2021-02-17 09:24:50 -0500

    reverse string video

---
+++ src/com/subtlealgorithm/strings_and_arrays/reverse_string/Solution.java
@@ -0,0 +1,18 @@
+package com.subtlealgorithm.strings_and_arrays.reverse_string;
+
+public class Solution {
+    public void reverseString(char[] s) {
+
+        int start = 0;
+        int end = s.length - 1;
+
+        while (start < end) {
+            char placeholder;
+            placeholder = s[start];
+            s[start] = s[end];
+            s[end] = placeholder;
+            end--;
+            start++;
+        }
+    }
+}
